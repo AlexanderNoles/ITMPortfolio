0ee19fd8c4a5937eae0b851f78a3f167cccb59a4
AlexanderNoles <alexnoles@outlook.com>
2022-10-29 15:28:24 +0100

    video page

---
+++ javascript/main.js
@@ -0,0 +1,37 @@
+
+var cssRoot = document.querySelector(':root');
+var onClickNavSound = new Audio("assets/click.wav");
+
+function changeToRed() //Only first function is commented as they all do the same thing
+{
+    //Set property in current page
+    cssRoot.style.setProperty('--primary-color', 'rgba(199, 43, 63, 1)');
+    //Save item to localStorage so colour choice can be reapplied when page is changed
+    localStorage.setItem('primary', 'rgba(199, 43, 63, 1)');
+    onNavClick();
+}
+
+function changeToGreen() 
+{
+    cssRoot.style.setProperty('--primary-color', 'rgba(31, 181, 71, 1)');
+    localStorage.setItem('primary', 'rgba(31, 181, 71, 1)');
+    onNavClick();
+}
+
+function changeToBlue() 
+{
+    cssRoot.style.setProperty('--primary-color', 'rgba(34, 95, 135, 1)');
+    localStorage.setItem('primary', 'rgba(34, 95, 135, 1)');
+    onNavClick();
+}
+
+window.onload = () =>
+{
+    cssRoot.style.setProperty('--primary-color', localStorage.getItem('primary'));
+};
+
+function onNavClick()
+{
+    onClickNavSound.currentTime = 0;
+    onClickNavSound.play();
+}
